c28df485a8c3b7ff5c936897084c9f23c7710078
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-03-19 19:32:33 +0300

    fix: add code at index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if(!Array.isArray(matrix)){
+    return []
+  }
+
+  result = []
+  matrix.forEach((element, index) => {
+    if (index % 2 == 1) {
+      element.reverse();
+    }
+    element.forEach(nubmer => {
+      result.push(nubmer);
+    });
+  });
+  return result;
 }
